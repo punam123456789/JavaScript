4ddd37fccc31e0434138c5d6fb2e17cb5c65aa26
punam123456789 <punamp073@gmail.com>
2023-04-05 17:51:41 +0530

    updated

---
+++ server.js
@@ -0,0 +1,63 @@
+var a = 2234;
+console.log(a);
+var a = 2
+console.log(2 == "2")
+console.log(2 != "2")
+console.log(2 === "2")
+
+if (2 > 1) {
+    console.log("sdhsfgsjkkj")
+    console.log("student")
+} else {
+    console.log("punam")
+}
+
+
+var userAge = 10;
+if (userAge > 80) {
+    console.log("You are over age for dl");
+}
+else if (userAge == 80) {
+    console.log("You are allow for learningL");
+
+}
+else if (userAge > 18) {
+    console.log("You are allow for DL");
+
+} else {
+    console.log("You are under age for dl");
+}
+
+var namee = "a";
+switch (namee) {
+    case "a":
+        console.log("You are a");
+        break;
+
+    case "b":
+        console.log("You are b");
+        break;
+    case "c":
+        console.log("You are c");
+        break;
+}
+
+var userA = 19;
+switch (true) {
+    case (userA>80):
+    console.log("You are over age for dl");
+    break;
+    case (userA==18):
+    console.log("You are allow for learningL");   
+    break;
+    
+    case (userA>18):
+        console.log("You are allow for DL");
+    break;
+    default:
+        console.log("You are under age for dl");
+
+    
+
+
+}
