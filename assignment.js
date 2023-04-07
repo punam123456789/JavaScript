5fbe7425c88f475f70e5f9929e36a62fb34e5762
punam123456789 <punamp073@gmail.com>
2023-04-07 18:15:16 +0530

    updated

---
+++ assignment.js
@@ -0,0 +1,87 @@
+// first...
+var number = 7;
+if(number%2==0){
+    console.log("number is evevn");
+}
+else{
+    console.log("number is odd");
+}
+
+// var number = 457;
+// switch(true){
+//     case(number%2==0):
+//     console.log("number is even");
+//     break;
+//     case(number%2!=0):
+//     console.log("number is odd");
+//     break;
+// }
+
+ 
+
+
+// second......
+
+// var num1=15;
+// var num2=15;
+// if(num1==num2){
+//     console.log("number is equals");
+// }
+//     else if(num1 > num2){
+//     console.log("number one is greater");
+// }
+// else{
+//     console.log("number one is small");
+
+// }
+
+
+// third.....
+
+// var num1=5;
+// var num2=10;
+// var num3=15;
+// if(num1>num2 && num1>num3){
+//     console.log("num1 is greater");
+// }
+// else if(num2>num1 && num2>num3){
+//     console.log("num2 is greater");
+
+// }
+// else{
+//     console.log("num3 is greater");
+// }
+
+// switch case
+// var num1=5;
+// var num2=10;
+//  var num3=15;
+//  switch(true){
+//     case(num1>num2):
+//     console.log("num1 is greater");
+//     break;
+//     case(num2>num3):
+//     console.log("num1 is greater");
+//     break;
+//     default:
+//         console.log("num3 is greater");
+
+//  }
+
+
+// 
+
+// forth.... 
+
+// var number = 4;
+// if(number>=5 && number<=25){
+//     console.log("number is find");
+// }
+// else{
+//     console.log ("number is not found");
+// }
+
+
+
+
+
